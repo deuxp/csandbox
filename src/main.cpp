0a6761361c0d9a7105b7fd037d737bf1e2e782f0
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-06-25 23:23:44 -0400

    init

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+// this imports the input/output libraries
+#include <iostream>
+
+// all c++ programs have one main() function
+int main()
+{
+  // init the storage location
+  int favourite_number;
+
+  // `<<` is the stream operator
+  std::cout << "Enter your favourite number 1-100";
+
+  // `>>` is the stream operator
+  std::cin >> favourite_number;
+
+  // std::endl is flushing the buffers and move the cursor to the next line
+  std::cout << "Amazing, that is my favourite number, too" << std::endl;
+
+  // this exits the program and everything is good
+  return 0;
+}
